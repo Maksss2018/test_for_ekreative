e7837f97f5590044d09450e11f2793df5c084826
Maksss2018 <malyimaksym37@gmail.com>
2019-01-16 11:13:40 +0200

    Promise  requests  and async for preloader etc Auth for user on  front end side (dont forgot to make  server and request for it) Redmin cros requests added

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,31 +5,24 @@ import {API,Users} from './config.json'
 import {
   REQ_SEND_ENTER, REQ_GET_ENTER, REQ_ERROR_ENTER
 } from '../constants/index.js'
+export const doRequest = (opt,parameters,requestFor) => {
+    return new Promise( (resolve, reject) => {
+            request.get(`${API}/${requestFor}.json${parameters}`,{
+                headers:{
+                    "X-Redmine-API-Key" : Users[opt.login][opt.password],
+                    "Content-Type": "application/json"
+                }},( err , res) => {
+                if (!err && res.statusCode == 200) {
+                    resolve(res.body);
+                } else {
+                    reject(err);
+                }
+            })
 
+    });
+};
 export const logingIn = (opt) => {
 //https://easyredmine.docs.apiary.io/#reference/issues/issues-collection/list-all-issues
-    const  doRequest =  (opt,parameters) => {
-        return new Promise( (resolve, reject) => {
-            console.log("login with :"+JSON.stringify(opt.login)+" "+opt.password);
-            if(Users[opt.login][opt.password]!==undefined){
-                request.get(`${API}/issues.json${parameters} `,{
-                    headers:{
-                        "X-Redmine-API-Key" : Users[opt.login][opt.password],
-                        "Content-Type": "application/json"
-                    }},( res, err) => {
-                    if (!err && res.statusCode == 200) {
-                        console.log("action rezult");
-                        resolve(res.body);
-                    } else {
-                        console.log("action rezult "+JSON.stringify(err));
-                        reject(err);
-                    }
-                })
-            }else{
-                reject("Unknown User! please sign-in first")
-            }
-        });
-    }
 
     return async (dispatch) => {
         let parameters = ["?sort=true","offset=0","limit=25","page=1"].join("&"),
@@ -47,8 +40,48 @@ export const logingIn = (opt) => {
             ]
         }
     };
-        doRequest(opt,parameters);
-
+        if(Users[opt.login][opt.password]!==undefined){
+            dispatch({
+                type: REQ_SEND_ENTER,
+                payload: {loading: true, error: false, auth: false}
+            });
+            try{
+                let rez= await  doRequest(opt,parameters,"projects");
+                dispatch({
+                    type: REQ_GET_ENTER,
+                    payload: {loading: false, error: false, auth: true, data:rez}
+                });
+            }catch(e){
+                console.log("ERROR!!!!!!");
+                dispatch({
+                    type: REQ_ERROR_ENTER,
+                    payload: {loading: false, error: true, auth: false}
+                });
+            }
+        }else{
+            dispatch({
+                type: REQ_ERROR_ENTER,
+                payload: {loading: false, error: true, auth: false}
+            });
+        }
+     /*
+     dispatch({
+            type: REQ_SEND_ENTER,
+            payload: {loading: true, error: false, auth: false}
+        });
+        try {
+            const rezult = await request(options);
+            dispatch({
+                type: REQ_GET_ENTER,
+                payload: {loading: false, error: false, auth: true, data:rezult}
+            });
+        } catch {
+            dispatch({
+                type: REQ_ERROR_ENTER,
+                payload: {loading: false, error: false, auth: true}
+            });
+        }
+     */
     }
 };
 /*
